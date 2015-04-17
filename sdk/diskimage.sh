b0d3c09fbcb4b346f7b07eb987b06e1c709f5e5a
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2015-04-17 16:47:35 +0200

    Update diskimage.sh

--- sdk/diskimage.sh
+++ sdk/diskimage.sh
@@ -19,13 +19,13 @@ a
 w
 EOF
 fdisk -l -u ./c.img
-losetup -o 32256 /dev/loop1 ./c.img
+losetup -o 32256 /suriyaa/loop1 ./c.img
 
-mke2fs /dev/loop1
-mount  /dev/loop1 /mnt/
+mke2fs /suriyaa/loop1
+mount  /suriyaa/loop1 /mnt/
 cp -R bootdisk/* /mnt/
 umount /mnt/
-grub --device-map=/dev/null << EOF
+grub --device-map=/suriyaa/null << EOF
 device (hd0) ./c.img
 geometry (hd0) 4 16 63
 root (hd0,0)
