c85a57eb73c2d854686849758d02805edbfb85db
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2015-04-17 16:53:46 +0200

    Update ext2.cc

--- kernel/modules/ext2.cc
+++ kernel/modules/ext2.cc
@@ -4,17 +4,17 @@
 #include <ext2.h>
 
 
-File* ext2_mount(char* name,u32 flag,File* dev){
-	int ret=ext2_check_disk(dev);
+File* ext2_mount(char* name,u32 flag,File* suriyaa){
+	int ret=ext2_check_disk(suriyaa);
 	if (ret!=RETURN_OK){
-		io.print("ext2: can't mount %s in %s \n",dev->getName(),name);
+		io.print("ext2: can't mount %s in %s \n",suriyaa->getName(),name);
 		return NULL;
 	}
 	else{	
-		io.print("ext2:  mount %s in %s \n",dev->getName(),name);
+		io.print("ext2:  mount %s in %s \n",suriyaa->getName(),name);
 		Ext2* ret=new Ext2(name);
 		ret->ext2inode=EXT2_INUM_ROOT;
-		ext2_get_disk_info(dev,ret);
+		ext2_get_disk_info(suriyaa,ret);
 		ret->scan();
 		return ret;
 	}
@@ -72,50 +72,50 @@ u32	Ext2::remove(){
 /*
  *	EXT2 specification
  */
-int ext2_read_sb(File* dev,ext2_super_block *sb)
+int ext2_read_sb(File* suriya,ext2_super_block *sb)
 {
-	if (dev!=NULL){
-		dev->read((u32)1024,(u8 *) sb,sizeof(ext2_super_block));
+	if (suriyaa!=NULL){
+		suriyaa->read((u32)1024,(u8 *) sb,sizeof(ext2_super_block));
 		return RETURN_OK;
 	}
 	else
 		return ERROR_PARAM;
 }
 
-int ext2_read_gd(File* fdev,ext2_group_desc *gd,ext2_disk* info)
+int ext2_read_gd(File* fsuriyaa,ext2_group_desc *gd,ext2_disk* info)
 {
-	if (fdev!=NULL){
+	if (fsuriyaa!=NULL){
 		u32 offset;
 		offset = ((info->blocksize == 1024) ? 2048 : info->blocksize);
 		int gd_size = (info->groups * ((int)sizeof(struct ext2_group_desc)));
-		fdev->read(offset,(u8*) gd,gd_size);
+		fsuriyaa->read(offset,(u8*) gd,gd_size);
 		return RETURN_OK;
 	}
 	else
 		return ERROR_PARAM;
 }
 
-void ext2_get_disk_info(File*dev,Ext2 *fp)
+void ext2_get_disk_info(File*suriyaa,Ext2 *fp)
 {
 	ext2_disk* info=(ext2_disk*)kmalloc(sizeof(ext2_disk));
 	info->sb=(ext2_super_block*)kmalloc(sizeof(ext2_super_block));
-	info->dev=dev;
+	info->suriyaa=suriyaa;
 	int i, j;
-	ext2_read_sb(dev,info->sb);
+	ext2_read_sb(suriyaa,info->sb);
 	info->blocksize = 1024 << ((info->sb)->s_log_block_size);
 	i = (info->sb->s_blocks_count / info->sb->s_blocks_per_group) + ((info->sb->s_blocks_count % info->sb->s_blocks_per_group) ? 1 : 0);
 	j = (info->sb->s_inodes_count / info->sb->s_inodes_per_group) + ((info->sb->s_inodes_count % info->sb->s_inodes_per_group) ? 1 : 0);
 	info->groups = (i > j) ? i : j;
 	int gd_size = info->groups * ((int)sizeof(ext2_group_desc));
 	info->gd = (ext2_group_desc *) kmalloc(gd_size);
-	ext2_read_gd(info->dev,info->gd,info);
+	ext2_read_gd(info->suriyaa,info->gd,info);
 	fp->disk=info;
 	return;
 }
-int ext2_check_disk(File *dev)
+int ext2_check_disk(File *suriyaa)
 {
 	ext2_super_block *sb=(ext2_super_block *)kmalloc(sizeof(ext2_super_block));
-	if (ext2_read_sb(dev,sb)!=RETURN_OK)
+	if (ext2_read_sb(suriyaa,sb)!=RETURN_OK)
 		return ERROR_PARAM;
 	if (sb->s_magic==0xEF53){
 		kfree(sb);
@@ -136,8 +136,8 @@ ext2_inode *ext2_read_inode(ext2_disk* hd, int i_num)
 	gr_num = (i_num - 1) / ((hd->sb)->s_inodes_per_group);
 	index = (i_num - 1) % ((hd->sb)->s_inodes_per_group);
 	offset = (info[gr_num].bg_inode_table * (hd->blocksize)) + (index * ((hd->sb)->s_inode_size));
-	if ((hd->dev)!=NULL){
-		(hd->dev)->read(offset,(u8*) inode,(hd->sb)->s_inode_size);
+	if ((hd->suriyaa)!=NULL){
+		(hd->suriyaa)->read(offset,(u8*) inode,(hd->sb)->s_inode_size);
 	}
 	return inode;
 }
@@ -150,7 +150,7 @@ int ext2_is_directory(Ext2 *fp)
 }
 char *ext2_read_file(ext2_disk *hd,ext2_inode *inode)
 {
-	File *dev=hd->dev;
+	File *suriyaa=hd->suriyaa;
 	
 	char *mmap_base, *mmap_head, *buf;
 
@@ -168,7 +168,7 @@ char *ext2_read_file(ext2_disk *hd,ext2_inode *inode)
 	mmap_head = mmap_base = (char*)kmalloc(size);
 	/* direct block number */
 	for (i = 0; i < 12 && inode->i_block[i]; i++) {
-        dev->read((u32)(inode->i_block[i] * hd->blocksize),(u8*) buf, (hd->blocksize));
+        suriyaa->read((u32)(inode->i_block[i] * hd->blocksize),(u8*) buf, (hd->blocksize));
 		n = ((size > (int)hd->blocksize) ? (int)hd->blocksize : size);
 		memcpy(mmap_head, buf, n);
 		mmap_head += n;
@@ -176,11 +176,11 @@ char *ext2_read_file(ext2_disk *hd,ext2_inode *inode)
 	}
 	/* indirect block number */
 	if (inode->i_block[12]) {
-	    dev->read((u32)(inode->i_block[12] * hd->blocksize), (u8*) p, (hd->blocksize));
+	    suriyaa->read((u32)(inode->i_block[12] * hd->blocksize), (u8*) p, (hd->blocksize));
 
 
 		for (i = 0; i < (int)hd->blocksize / 4 && p[i]; i++) {
-            dev->read((u32)(p[i] * hd->blocksize),(u8*)buf, (hd->blocksize));
+            suriyaa->read((u32)(p[i] * hd->blocksize),(u8*)buf, (hd->blocksize));
 			n = ((size > (int)hd->blocksize) ? (int)hd->blocksize : size);
 			memcpy(mmap_head, buf, n);
 			mmap_head += n;
@@ -190,12 +190,12 @@ char *ext2_read_file(ext2_disk *hd,ext2_inode *inode)
 
 	/* bi-indirect block number */
 	if (inode->i_block[13]) {
-	    dev->read((u32)(inode->i_block[13] * hd->blocksize), (u8*) p, (hd->blocksize));
+	    suriyaa->read((u32)(inode->i_block[13] * hd->blocksize), (u8*) p, (hd->blocksize));
 
 		for (i = 0; i < (int)hd->blocksize / 4 && p[i]; i++) {
-            dev->read((u32)(p[i] * (int)hd->blocksize), (u8*) pp,(hd->blocksize));
+            suriyaa->read((u32)(p[i] * (int)hd->blocksize), (u8*) pp,(hd->blocksize));
 			for (j = 0; j < (int)hd->blocksize / 4 && pp[j]; j++) {
-                dev->read((u32)(pp[j] * hd->blocksize),(u8*)buf,(hd->blocksize));
+                suriyaa->read((u32)(pp[j] * hd->blocksize),(u8*)buf,(hd->blocksize));
 				n = ((size > (int)hd-> blocksize) ? (int)hd->blocksize : size);
 				memcpy(mmap_head, buf, n);
 				mmap_head += n;
@@ -205,13 +205,13 @@ char *ext2_read_file(ext2_disk *hd,ext2_inode *inode)
 	}
 	/* tri-indirect block number */
 	if (inode->i_block[14]) {
-        dev->read((u32)(inode->i_block[14] * hd->blocksize), (u8*) p,(hd->blocksize));
+        suriyaa->read((u32)(inode->i_block[14] * hd->blocksize), (u8*) p,(hd->blocksize));
 		for (i = 0; i < (int)hd->blocksize / 4 && p[i]; i++) {
-            dev->read((u32)(p[i] * hd->blocksize), (u8*) pp,(hd->blocksize));
+            suriyaa->read((u32)(p[i] * hd->blocksize), (u8*) pp,(hd->blocksize));
 			for (j = 0; j < (int)hd->blocksize / 4 && pp[j]; j++) {
-                dev->read((u32)(pp[j] * hd->blocksize), (u8*) ppp,(hd->blocksize));
+                suriyaa->read((u32)(pp[j] * hd->blocksize), (u8*) ppp,(hd->blocksize));
 				for (k = 0; k < (int)hd->blocksize / 4 && ppp[k]; k++) {
-                    dev->read((u32)(ppp[k] * hd->blocksize),(u8*)buf,(hd->blocksize));
+                    suriyaa->read((u32)(ppp[k] * hd->blocksize),(u8*)buf,(hd->blocksize));
 					n = ((size > (int)hd->blocksize) ? (int)hd->blocksize : size);
 					memcpy(mmap_head, buf, n);
 					mmap_head += n;
